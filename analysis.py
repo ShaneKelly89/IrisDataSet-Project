aa5ad80fffaaad2669d36c94c28d2fc549378730
Shane Kelly <G00398368@gmit.ie>
2021-04-11 14:03:27 -0700

    adding update

--- analysis.py
+++ analysis.py
@@ -27,9 +27,6 @@ print("\n")
 print("The data places variables evenly into three species of Iris Flower:")
 print(dataset.groupby('species').size())
 print("\n")
-print("We can also take a look at the statistical summary of the data:")
-print(dataset.describe())
-print("\n")
 #Now I will read the file for NumPy 
 #https://stackoverflow.com/questions/3518778/how-do-i-read-csv-data-into-a-record-array-in-numpy
 data = np.genfromtxt(file, delimiter=',')
@@ -56,8 +53,9 @@ petalWidth = data[:,3]
 #https://jakevdp.github.io/PythonDataScienceHandbook/02.04-computation-on-arrays-aggregates.html
 #with our newly created variables above we can run stats for the min, max, mean, and st. dev for each 
 
+print("As seen above, the dataset contains numeric data which is held within four seperate columns")
 #Sepal Lenght stats 
-print("Below are some statistics of the 'Sepal Lenght' variable within the Iris Dataset")
+print("Below are some statistics of the 'Sepal Lenght' column within the dataset")
 slMin = np.min(sepalLenght)
 print("Sepal Lenght minimum:",slMin)
 slMax = np.max(sepalLenght)
@@ -67,8 +65,38 @@ print("Sepal Lenght mean:",slMean)
 slStd = np.std(sepalLenght)
 print("Sepal Lenght standard deviation:",slStd)
 
+print("\n")
+#Sepal Width stats 
+print("Below are some statistics of the 'Sepal Width' column within the dataset")
+swMin = np.min(sepalWidth)
+print("Sepal Width min:",swMin)
+swMax = np.max(sepalWidth)
+print("Sepal Width max:",swMax)
+swMean = np.mean(sepalWidth)
+print("Sepal With mean:",swMean)
+swStd = np.std(sepalWidth)
+print("Sepal Width standard deviation:",swStd)
 
+print("\n")
+#Petal Lenght stats
+print("Below are some statistics of the 'Petal Lenght' column within the dataset")
+plMin = np.min(petalLenght)
+print("Petal Lenght minimum:",plMin)
+plMax = np.max(petalLenght)
+print("Petal Lenght max:",plMax)
+plMean = np.mean(petalLenght)
+print("Petal Lenght mean:",plMean)
+plStd = np.std(petalLenght)
+print("Petal Lenght standard deviation:",plStd)
 
-
-
-
+print("\n")
+#Petal Width stats
+print("Below are some statistics of the 'Petal Width' column within the dataset")
+pwMin = np.min(petalWidth)
+print("Petal Width min:",pwMin)
+pwMax = np.max(petalWidth)
+print("Petal Width max:",pwMax)
+pwMean = np.mean(petalWidth)
+print("Petal With mean:",pwMean)
+pwStd = np.std(petalWidth)
+print("Petal Width standard deviation:",pwStd)
